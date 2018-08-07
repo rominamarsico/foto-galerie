5eaef37bb92843ca21816fbb0f00b3eae72df6f8
Romina Marsico <rominamarsico@users.noreply.github.com>
2018-08-07 23:14:51 +0200

    firebase file uploader

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,64 @@
-import React from 'react';
+import React, { Component } from "react";
+import firebase from "firebase";
+import FileUploader from "react-firebase-file-uploader";
 
-const Home = () =>
-  <div>
-    <h1>Home</h1>
-  </div>
+class Home extends Component {
+  state = {
+    username: "",
+    avatar: "",
+    isUploading: false,
+    progress: 0,
+    avatarURL: ""
+  };
+
+  handleChangeUsername = event =>
+    this.setState({ username: event.target.value });
+  handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
+  handleProgress = progress => this.setState({ progress });
+  handleUploadError = error => {
+    this.setState({ isUploading: false });
+    console.error(error);
+  };
+  handleUploadSuccess = filename => {
+    this.setState({ avatar: filename, progress: 100, isUploading: false });
+    firebase
+      .storage()
+      .ref("images")
+      .child(filename)
+      .getDownloadURL()
+      .then(url => this.setState({ avatarURL: url }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+          <form>
+            <label>Username:</label>
+            <input
+              type="text"
+              value={this.state.username}
+              name="username"
+              onChange={this.handleChangeUsername}
+            />
+            <label>Avatar:</label>
+            {this.state.isUploading && <p>Progress: {this.state.progress}</p>}
+            {this.state.avatarURL && <img src={this.state.avatarURL} alt="avatar"/>}
+            <FileUploader
+              accept="image/*"
+              name="avatar"
+              randomizeFilename
+              storageRef={firebase.storage().ref("images")}
+              onUploadStart={this.handleUploadStart}
+              onUploadError={this.handleUploadError}
+              onUploadSuccess={this.handleUploadSuccess}
+              onProgress={this.handleProgress}
+              multiple="true"
+            />
+          </form>
+      </div>
+    );
+  }
+}
 
 export default Home;
