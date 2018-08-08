bf4d63bdeff6136d137886ad64a341cb9b82cd71
Romina Marsico <rominamarsico@users.noreply.github.com>
2018-08-08 17:41:43 +0200

    include messages in database

--- src/components/Home.js
+++ src/components/Home.js
@@ -27,35 +27,50 @@ class Home extends Component {
       .child(filename)
       .getDownloadURL()
       .then(url => this.setState({ avatarURL: url }));
+
+    var urlArray = [];
+    urlArray.push(this.state.avatarURL)
+    // console.log(urlArray)
+    // console.log("Array Länge: " + urlArray.length)
+    console.log("BITTEEEE: " + firebase.storage.Reference.bucket)
   };
 
   render() {
+
+    // firebase.storage().ref("images").child('44be445b-d4c0-4579-b0a3-86ab99b8915a.JPG').getDownloadURL().then(function(url) {
+    //   var img = document.getElementById('gallery');
+    //   img.src = url;
+    // })
+
     return (
       <div>
         <h1>Home</h1>
-          <form>
-            <label>Username:</label>
-            <input
-              type="text"
-              value={this.state.username}
-              name="username"
-              onChange={this.handleChangeUsername}
-            />
-            <label>Avatar:</label>
-            {this.state.isUploading && <p>Progress: {this.state.progress}</p>}
-            {this.state.avatarURL && <img src={this.state.avatarURL} alt="avatar"/>}
-            <FileUploader
-              accept="image/*"
-              name="avatar"
-              randomizeFilename
-              storageRef={firebase.storage().ref("images")}
-              onUploadStart={this.handleUploadStart}
-              onUploadError={this.handleUploadError}
-              onUploadSuccess={this.handleUploadSuccess}
-              onProgress={this.handleProgress}
-              multiple="true"
-            />
-          </form>
+        <form>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={this.state.username}
+            name="username"
+            onChange={this.handleChangeUsername}
+          />
+          <label>Avatar:</label>
+          {this.state.isUploading && <p>Progress: {this.state.progress}</p>}
+          <FileUploader
+            accept="image/*"
+            name="avatar"
+            storageRef={firebase.storage().ref("images")}
+            onUploadStart={this.handleUploadStart}
+            onUploadError={this.handleUploadError}
+            onUploadSuccess={this.handleUploadSuccess}
+            onProgress={this.handleProgress}
+            multiple="true"
+          />
+        <br /> <br />
+        {this.state.avatarURL && <img src={this.state.avatarURL} alt="avatar" width="30%"/>}
+        </form>
+        {/*
+          <img id="gallery" alt="gallery" width="30%"/>
+        */}
       </div>
     );
   }
